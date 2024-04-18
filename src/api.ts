560966e01c908b297591f50010c458484dc3b332
MilagrosVerde <milagrosverde1234@gmail.com>
2024-04-18 16:36:33 -0300

    first commit

--- src/api.ts
+++ src/api.ts
@@ -126,8 +126,26 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const api = {
   list: async (): Promise<Restaurant[]> => {
-    await sleep(750);
+    // Obtenemos la información de Google Sheets en formato texto y la dividimos por líneas, nos saltamos la primera línea porque es el encabezado
+    const [, ...data] = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vR_4pGF-dyskUp_X09es4NepFYII7CzWG-TDGQT9PVqVwVlyOxxvpjiKvxBrauPZtSdVKod2-5M6l4H/pub?output=csv")
+    .then(res => res.text())
+    .then(text => text.split('\n'))
 
+    // Convertimos cada línea en un objeto Restaurant, asegúrate de que los campos no posean `,`
+    const restaurants: Restaurant[] = data.map((row) => {
+      const [id, name, description, address, score, ratings, image] = row.split(',')
+      return {
+        id,
+        name,
+        description,
+        address,
+        score: Number(score),
+        ratings: Number(ratings),
+        image
+      }
+    })
+
+    // Lo retornamos
     return restaurants;
   },
   fetch: async (id: Restaurant["id"]): Promise<Restaurant> => {
